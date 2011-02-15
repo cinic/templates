80b1ac84dcb375418cd2996cd8fe1d5e4ed6abe1
cinic <cinic.rus@gmail.com>
2011-02-16 00:36:56 +0300

    add files

---
+++ broco/js/js.js
@@ -0,0 +1,7 @@
+document.createElement("section");
+document.createElement("header");
+document.createElement("aside");
+document.createElement("footer");
+document.createElement("menu");
+document.createElement("nav");
+document.createElement("time");
