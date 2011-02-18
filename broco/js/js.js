9f59ab9a45782724b2a545220b6ca8082788de92
cinic <cinic.rus@gmail.com>
2011-02-18 19:21:15 +0300

    Added styles for promo blocks & js for promo blocks

--- broco/js/js.js
+++ broco/js/js.js
@@ -1,7 +1,19 @@
-document.createElement("section");
-document.createElement("header");
-document.createElement("aside");
-document.createElement("footer");
-document.createElement("menu");
-document.createElement("nav");
-document.createElement("time");
+document.documentElement.id = "js";
+$(function(){
+	/*Promo Block control elements*/
+	var objHead = document.getElementsByTagName('head');
+  	if (objHead[0])
+  	{
+    	var objCSS = objHead[0].appendChild(document.createElement('link'));
+    	objCSS.id = 'three-promo-box-css';
+    	objCSS.rel = 'stylesheet';
+    	objCSS.href = '/assets/templates/broco/css/promo.blocks/three.promo.box.css';
+  	}
+	
+	$("#promo div").addClass("promo-control");
+	$("#promo .promo-control").click(function(){
+		$(this).removeClass("narrowed").addClass("expanded").siblings().removeClass("expanded").addClass("narrowed");
+	});
+	
+	$('.promo-control').children('section').children('p').wrapInner('<span class="c"/>').wrapInner('<span class="r"/>').wrapInner('<span class="l"/>');
+});
