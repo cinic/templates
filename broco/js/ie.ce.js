48db57d9f647dcd0cec6897ba82060cc80f8a9fa
cinic <cinic.rus@gmail.com>
2011-03-01 02:57:17 +0300

    Updated ie.ce.js added create element ARTICLE

--- broco/js/ie.ce.js
+++ broco/js/ie.ce.js
@@ -1,6 +1,7 @@
 document.createElement("section");
 document.createElement("header");
 document.createElement("aside");
+document.createElement("article");
 document.createElement("footer");
 document.createElement("menu");
 document.createElement("nav");
